39e8aa6560c314e4445591469a50e199cac0e888
Ftbom <lz490070@gmail.com>
2025-03-07 18:36:09 +0800

    Update gfmanhua.js

--- javascript/manga/src/zh/gfmanhua.js
+++ javascript/manga/src/zh/gfmanhua.js
@@ -7,7 +7,7 @@ const mangayomiSources = [{
     "typeSource": "single",
     "itemType": 0,
     "version": "0.0.1",
-    "pkgPath": ""
+    "pkgPath": "manga/src/zh/gfmanhua.js"
 }];
 
 class DefaultExtension extends MProvider {
