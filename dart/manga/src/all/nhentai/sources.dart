498f824aae5121ba0bf41cf0383fd8c0ca46525a
Ftbom <lz490070@gmail.com>
2025-04-27 17:38:27 +0800

    Update sources.dart

--- dart/manga/src/all/nhentai/sources.dart
+++ dart/manga/src/all/nhentai/sources.dart
@@ -2,12 +2,13 @@ import '../../../../../model/source.dart';
 
 const _nhentaiVersion = "0.0.15";
 const _nhentaiSourceCodeUrl =
-    "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/dart/manga/src/all/nhentai/nhentai.dart";
+    "https://raw.githubusercontent.com/Ftbom/mangayomi-extensions-ex/$branchName/dart/manga/src/all/nhentai/nhentai.dart";
 
 String _iconUrl =
-    "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/dart/manga/src/all/nhentai/icon.png";
+    "https://raw.githubusercontent.com/Ftbom/mangayomi-extensions-ex/$branchName/dart/manga/src/all/nhentai/icon.png";
 const _baseUrl = 'https://nhentai.net';
 const _isNsfw = true;
+const _itemType = ItemType.manga;
 
 List<String> _languages = ["all", "en", "zh", "ja"];
 
@@ -21,6 +22,7 @@ List<Source> _nhentaiSourcesList = _languages
         typeSource: "NHentai",
         iconUrl: _iconUrl,
         isNsfw: _isNsfw,
+        itemType: _itemType,
         version: _nhentaiVersion,
         sourceCodeUrl: _nhentaiSourceCodeUrl))
     .toList();
